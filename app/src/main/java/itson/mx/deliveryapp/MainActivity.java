fa7d95e3c70be66bc4f2af5ae222d6c15c2778b0
ferchaparro <chaparro3214@gmail.com>
2015-10-09 17:17:55 -0700

    .

--- app/src/main/java/itson/mx/deliveryapp/MainActivity.java
+++ app/src/main/java/itson/mx/deliveryapp/MainActivity.java
@@ -1,14 +1,20 @@
 package itson.mx.deliveryapp;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Map;
 
@@ -21,11 +27,15 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recycler;
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager layourManager;
+    private ProgressBar progress;
+    private ImageView promo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
 
         recycler = (RecyclerView) findViewById(R.id.my_recycler_view);
         recycler.setHasFixedSize(true);
@@ -33,6 +43,40 @@ public class MainActivity extends AppCompatActivity {
         recycler.setLayoutManager(layourManager);
         adapter = new ButtonRecycler(getDataSet());
         recycler.setAdapter(adapter);
+        progress = (ProgressBar)findViewById(R.id.progress_main);
+        promo = (ImageView) findViewById(R.id.img_promo);
+
+        progress.setVisibility(View.VISIBLE);
+        progress.setIndeterminate(true);
+
+        AsyncTask<String, Void, Bitmap> task = new AsyncTask<String, Void, Bitmap>() {
+            @Override
+            protected Bitmap doInBackground(String... params) {
+                try {
+                    URL url = new URL(params[0]);
+                    Bitmap loadedImage = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+                    return loadedImage;
+                } catch (Exception e) {
+                    return null;
+                }
+            }
+
+            @Override
+            protected void onPostExecute(Bitmap bitmap) {
+                if (bitmap == null) {
+                    Toast.makeText(MainActivity.this, "No se cargo la promocion", Toast.LENGTH_SHORT).show();
+                }
+                promo.setImageBitmap(bitmap);
+                progress.setVisibility(View.INVISIBLE);
+            }
+        };
+        try {
+            task.execute("https://scontent-lax3-1.xx.fbcdn.net/hphotos-xal1/v/t1.0-9/12105773_1665297650373502_3045004077835436874_n.jpg?oh=13922bac899ae4bc2a20b663806ad5ec&oe=56D1D333");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
 
     }
 
